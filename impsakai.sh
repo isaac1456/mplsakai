c016c644e4c11d012c81a0cf52bc43f947e60785
ISAAC FLOREZ JIMENEZ <motato1456@gmail.com>
2019-03-18 09:59:01 -0500

    Create impsakai.sh

---
+++ impsakai.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Install the Sakai master project
+cd /sakai0/sakai/master;
+mvn clean install > /sakai0/logs-implementacion/ins-sakai.log;
+# mvn clean install -Dmaven.test.skip=true sakai:deploy
+
+# Install and deploy Sakai
+cd /sakai0/sakai;
+mvn clean install sakai:deploy -Dmaven.tomcat.home=/sakai0/tomcat -Dsakai.home=/sakai0/tomcat/sakai -Djava.net.preferIPv4Stack=true -Dmaven.test.skip=true > /sakai0/logs-implementacion/imp-sakai.log;
+# -Dsakai.home=/sakai0/tomcat/sakai
