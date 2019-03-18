1bfe67cb3c4fec5b5a23996c0ba2fcaa850c40aa
ISAAC FLOREZ JIMENEZ <motato1456@gmail.com>
2019-03-18 10:47:43 -0500

    Update impsakai.sh

--- impsakai.sh
+++ impsakai.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+sed -i "777c portal.iframesuppress=:all:sakai.scorm.singlepackage.tool:sakai.scorm.tool" /sakai0/tomcat/sakai/sakai.properties;
 # Install the Sakai master project
 cd /sakai0/sakai/master;
 mvn clean install > /sakai0/logs-implementacion/ins-sakai.log;
